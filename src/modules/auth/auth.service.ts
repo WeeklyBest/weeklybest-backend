9150ab27441ae8739b38380ada300f931e392c55
marshot <124557537+marshot9420@users.noreply.github.com>
2023-09-10 23:46:34 +0900

    [Docs] 이메일 회원가입 API 문서 추가

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import * as bcrypt from 'bcrypt';
+
+import { User, UserRepository, UserRole } from '@/models';
+
+import { AUTH, AUTH_ERROR } from './auth.constant';
+import { JoinForm } from './dtos';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  async join(joinForm: JoinForm): Promise<void> {
+    const { email, password, name, mobile } = joinForm;
+
+    const exUser: User = await this.userRepository.findOne({
+      where: { email },
+    });
+
+    if (exUser) {
+      throw new HttpException(
+        AUTH_ERROR.DUPLICATE_EMAIL,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    try {
+      await this.userRepository.save(
+        this.userRepository.create({
+          email,
+          password: await bcrypt.hash(password, AUTH.SALT),
+          name,
+          mobile,
+          role: UserRole.USER,
+        }),
+      );
+    } catch (err) {
+      throw new HttpException(
+        AUTH_ERROR.JOIN_ERROR,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
