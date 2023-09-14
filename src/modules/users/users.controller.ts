eeb7c58113a17f3ebdf18720f69302f70e0bd062
marshot <124557537+marshot9420@users.noreply.github.com>
2023-09-14 12:16:32 +0900

    [Docs] 사용자 본인 정보 조회 태그

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,8 +1,12 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { CurrentUser, JwtAuthGuard } from '@/modules/auth';
+import { ApiTags } from '@nestjs/swagger';
+
 import { User } from '@/models';
+import { CurrentUser, JwtAuthGuard } from '@/modules/auth';
+
 import { UserSummary } from './dtos';
 
+@ApiTags('사용자 본인 정보 조회')
 @Controller('users')
 export class UsersController {
   @Get('me')
