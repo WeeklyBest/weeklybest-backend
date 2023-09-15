9834c04190dd34a1b662d5bf8971ab686e34d927
marshot <124557537+marshot9420@users.noreply.github.com>
2023-09-15 10:42:16 +0900

    [Docs] 상품 목록 조회 API 문서 작성

---
+++ src/modules/products/products.controller.docs.ts
@@ -0,0 +1,18 @@
+import { applyDecorators } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+
+import { SwaggerMethodDocType } from '@/common';
+
+import { ProductsController } from './products.controller';
+
+export const ProductsControllerDocs: SwaggerMethodDocType<ProductsController> =
+  {
+    list(summary: string) {
+      return applyDecorators(
+        ApiOperation({
+          summary,
+          description: '진열 여부가 true인 상품 목록을 모두 조회합니다.',
+        }),
+      );
+    },
+  };
--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -4,12 +4,14 @@ import { ApiTags } from '@nestjs/swagger';
 import { Product } from '@/models';
 
 import { ProductsService } from './products.service';
+import { ProductsControllerDocs as Docs } from './products.controller.docs';
 
 @ApiTags('상품 API')
 @Controller('product')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @Docs.list('상품 목록 조회')
   @Get()
   async list(): Promise<Product[]> {
     return this.productsService.list();
