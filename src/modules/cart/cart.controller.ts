927219cef1edb2ea1f210b26418109df259d6593
marshot <124557537+marshot9420@users.noreply.github.com>
2023-09-18 13:49:11 +0900

    [Docs] 장바구니 상품 추가 API 문서 작성

--- src/modules/cart/cart.controller.ts
+++ src/modules/cart/cart.controller.ts
@@ -4,12 +4,14 @@ import { CartService } from './cart.service';
 import { CurrentUser, JwtAuthGuard } from '../auth';
 import { CreateCartDto } from './dtos';
 import { User } from '@/models';
+import { CartControllerDocs as Docs } from './cart.controller.docs';
 
 @ApiTags('장바구니 API')
 @Controller('cart')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
 
+  @Docs.create('장바구니에 상품 추가')
   @Post()
   @UseGuards(JwtAuthGuard)
   async create(@Body() dto: CreateCartDto, @CurrentUser() user: User) {
