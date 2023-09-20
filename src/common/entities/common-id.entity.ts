26c81f7d9597705aadc360cbc2c45c9537acc507
marshot <124557537+marshot9420@users.noreply.github.com>
2023-09-20 10:09:52 +0900

    [Refactor] Common Entity Swagger 데코레이터 구조 변경

--- src/common/entities/common-id.entity.ts
+++ src/common/entities/common-id.entity.ts
@@ -2,10 +2,7 @@ import { PrimaryGeneratedColumn } from 'typeorm';
 
 import { CommonEntity } from './common.entity';
 
-import { SwaggerDoc } from '../doc';
-
 export abstract class CommonIdEntity extends CommonEntity {
-  @SwaggerDoc.id('식별자')
   @PrimaryGeneratedColumn({
     unsigned: true,
   })
--- src/common/entities/common.entity.ts
+++ src/common/entities/common.entity.ts
@@ -1,20 +1,9 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
-
 import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 export abstract class CommonEntity {
-  @ApiPropertyOptional({
-    example: '2023-09-10 18:00:32',
-    description: '생성일',
-    required: true,
-  })
   @CreateDateColumn()
   createdAt?: Date;
 
-  @ApiPropertyOptional({
-    example: '2023-09-11 12:36:22',
-    description: '수정일',
-  })
   @UpdateDateColumn()
   updatedAt?: Date;
 }
