2b9714e16442585a7e9081632bc63a5d90c7f854
marshot <124557537+marshot9420@users.noreply.github.com>
2023-10-05 08:28:27 +0900

    [Feat] 이메일 가입 회원 탈퇴 기능 추가

--- src/models/entities/customer/user.entity.ts
+++ src/models/entities/customer/user.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { Column, DeleteDateColumn, Entity, ManyToOne } from 'typeorm';
 
 import { CommonIdEntity } from '@/common';
 import { SNSProvider, USER, UserRole, Wishlist } from '@/models';
@@ -53,6 +53,9 @@ export class User extends CommonIdEntity {
     select: false,
   })
   refreshToken: string;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
 
   // 관계 설정
   @ManyToOne(() => Wishlist, (wishlist) => wishlist.user)
