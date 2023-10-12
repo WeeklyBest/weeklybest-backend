012ea9fd236608a2ce1dd862130afca684985272
marshot <124557537+marshot9420@users.noreply.github.com>
2023-10-12 17:51:59 +0900

    [Refactor] 상품 상세 조회 쿼리 Service → Repository로 이동

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -87,24 +87,7 @@ export class ProductsService {
   }
 
   async getOne(productId: number, user: User): Promise<ProductDetailResponse> {
-    const [productAlias, productImageAlias, variantAlias] = [
-      'product',
-      'image',
-      'variant',
-      'wishlist',
-    ];
-
-    const product = await this.productRepository
-      .createQueryBuilder(productAlias)
-      .leftJoinAndSelect(`${productAlias}.images`, productImageAlias)
-      .where(`${productAlias}.id = :productId`, { productId })
-      .leftJoinAndSelect(
-        `${productAlias}.variants`,
-        variantAlias,
-        `${variantAlias}.product.id = ${productAlias}.id AND ${variantAlias}.hide = false`,
-      )
-      .orderBy(`${productImageAlias}.order`, 'ASC')
-      .getOne();
+    const product = await this.productRepository.getProductDetail(productId);
 
     throwExceptionOrNot(product, EXCEPTION.PRODUCT.NOT_FOUND);
 
